8ac59ad35d65a21ed6b51be7b024b80b260799c7
Jared <jared.michael.wood@gmail.com>
2018-03-29 20:09:58 -0500

    Finished username and password error messages

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import jinja2
+from flask import Flask, redirect, request, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -7,5 +6,54 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
     return render_template('signup_form.html')
+
+@app.route('/', methods=['POST'])
+def submitted():
+
+    username = request.form['username']
+    password = request.form['password']
+    v_password = request.form['v_password']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    v_password_error = ''
+    email_error = ''
+
+    if username == '':
+        username_error = 'Please enter a Username.'
+    if len(username) < 3 or len(username) > 20:
+        username_error = 'Your username must be more than 3 characters and less than 20 characters.'
+    if ' ' in username:
+        username_error = 'Your username cannot contain any spaces.'
+
+    if password == '':
+        password_error = 'Please enter a Password.'
+    if len(password) < 3 or len(password) > 20:
+        password_error = 'Your username must be more than 3 characters and less than 20 characters.'
+    if ' ' in password:
+        password_error = 'Your password cannot contain any spaces.'
+
+    if v_password == '':
+        v_password_error = 'Please re-enter your Password.'
+
+    if password != v_password:
+        password_error = ''
+        v_password_error = 'Please verify with a correct password.'
+
+    # TODO:
+    # add email conditions to validation
+
+    if username_error == '':
+        return redirect('/welcome')
+
+    else:
+        return render_template('signup_form.html', username=username, email=email,
+            username_error=username_error, password_error=password_error, v_password_error=v_password_error,
+            email_error=email_error)
+
+@app.route('/welcome')
+def welcome():
+    return '<h1>Welcome!</h1>'
 
 app.run()
